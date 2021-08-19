f1f17771756c6aa31aa86daa7140678df9023eef
Muskan <muskanchhatrasal@gmail.com>
2021-08-19 16:58:25 +0530

    Fusion Charts - Doughnut Chart

--- starter-project-react-github-search-users-master/src/components/Repos.js
+++ starter-project-react-github-search-users-master/src/components/Repos.js
@@ -37,7 +37,9 @@ const Repos = () => {
   <section className='section'>
     <Wrapper className='section-center'>
       <Pie3D data={languages} />
-       {/* <ExampleChart data={chartData}/> */}
+      <div></div>
+      <Doughnut2D data={chartData} />
+      <div></div>
     </Wrapper>
   </section>
   );
