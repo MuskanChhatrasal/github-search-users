6349c63c40a05726e7281fe343dc41d0b922c81b
Muskan <muskanchhatrasal@gmail.com>
2021-08-20 16:04:18 +0530

    Error

--- starter-project-react-github-search-users-master/src/context/context.js
+++ starter-project-react-github-search-users-master/src/context/context.js
@@ -15,6 +15,8 @@ const GithubProvider = ({children})=>{
     //requests loading
     const [requests,setRequests] = useState(0);
     const [loading,setLoading] = useState(false);
+    //error
+    const [error,setError] = useState({show:false,msg:""});
     //check rate
     const checkRequests = ()=>{
         axios(`${rootUrl}/rate_limit`)
@@ -23,14 +25,17 @@ const GithubProvider = ({children})=>{
         } = data;
         setRequests(remaining);
         if(remaining === 0){
-            //throw an error
+            toggleError(true,"sorry, you have exceeded your hourly rate limit!");
         }
         })
         .catch((err)=>console.log(err))
+    };
+    function toggleError(show = false,msg = ''){
+        setError({show,msg})
     }
     //error
     useEffect(checkRequests,[]);
-    return <GithubContext.Provider value={{githubUser,repos,followers,requests}}>{children}</GithubContext.Provider>
+    return <GithubContext.Provider value={{githubUser,repos,followers,requests,error}}>{children}</GithubContext.Provider>
 }
 
 export {GithubProvider,GithubContext}
