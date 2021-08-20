cc18ef46d90c94d6b923301ad2115eb6d20533a7
Muskan <muskanchhatrasal@gmail.com>
2021-08-20 15:48:47 +0530

    Requests

--- starter-project-react-github-search-users-master/src/components/Search.js
+++ starter-project-react-github-search-users-master/src/components/Search.js
@@ -4,6 +4,8 @@ import { MdSearch } from 'react-icons/md';
 import { GithubContext } from '../context/context';
 const Search = () => {
   const [user,setUser] = React.useState('');
+  const {requests} = React.useContext(GithubContext);
+  console.log(requests);
   //get things from global context
   const handleSubmit = (e) =>{
     e.preventDefault();
@@ -20,10 +22,10 @@ const Search = () => {
          <div className='form-control'>
             <MdSearch/>
             <input type='text' placeholder='enter github user' value={user} onChange={(e)=>setUser(e.target.value)}/>
-            <button type='submit'>Search</button>
+            {requests > 0 && <button type='submit'>Search</button>}
          </div>
        </form>
-       <h3>requests : 60 / 60</h3>
+       <h3>requests : {requests} / 60</h3>
     </Wrapper>
   </section>
   );
