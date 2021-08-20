262aa73f4d3cc2ecba464c137daf43efc7d8ea6c
Muskan <muskanchhatrasal@gmail.com>
2021-08-20 16:31:00 +0530

    Search User

--- starter-project-react-github-search-users-master/src/context/context.js
+++ starter-project-react-github-search-users-master/src/context/context.js
@@ -17,6 +17,20 @@ const GithubProvider = ({children})=>{
     const [loading,setLoading] = useState(false);
     //error
     const [error,setError] = useState({show:false,msg:""});
+    
+    const searchGithubUser = async(user)=>{
+        toggleError();
+        //setLoading(true)
+        const response = await axios(`${rootUrl}/users/${user}`).catch(err=>console.log(err));
+        console.log(response);
+        if(response){
+            setgithubUser(response.data);
+        }else{
+            toggleError(true,'there is no user with that username');
+        }
+    }
+
+
     //check rate
     const checkRequests = ()=>{
         axios(`${rootUrl}/rate_limit`)
@@ -35,7 +49,7 @@ const GithubProvider = ({children})=>{
     }
     //error
     useEffect(checkRequests,[]);
-    return <GithubContext.Provider value={{githubUser,repos,followers,requests,error}}>{children}</GithubContext.Provider>
+    return <GithubContext.Provider value={{githubUser,repos,followers,requests,error,searchGithubUser}}>{children}</GithubContext.Provider>
 }
 
 export {GithubProvider,GithubContext}
